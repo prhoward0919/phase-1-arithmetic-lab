6601c30c428210af2f784c166c5dffd598667fb9
Paige <prhoward97@gmail.com>
2022-09-24 17:00:54 -0400

    commit tree a1e9b2a39f04942c7f29135d089f860ad35e82ae

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 40;
+const num4 = 6;
+
+/*function multiply() {
+    num1 * num2;
+ 
+}*/
+
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random()*10) + 1;
+const mod = num3 % num4;
+const max = Math.max(1,2,3,4,5,6,7,8,9,20);
